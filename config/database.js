396d1c4605b560d825d73e366ae4fe0059b298f9
evarela4mayor <eddievarelaformayor@gmail.com>
2025-07-11 22:38:17 -0600

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,12 @@
+```javascript
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+    },
+    useNullAsDefault: true,
+    debug: false,
+  },
+});
+```
