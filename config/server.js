0a78062e8e7a67d59259c8f31d0d99ef72444b85
evarela4mayor <eddievarelaformayor@gmail.com>
2025-07-13 12:10:48 -0600

    Update server.js
    Add the allowedHosts configuration to your server.js.

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('STRAPI_PUBLIC_URL', 'https://eddie-varela-cms.onrender.com'),
+  allowedHosts: ['eddie-varela-cms.onrender.com'],
   app: {
     keys: env.array('APP_KEYS'),
   },
