b4696e83fe135011db84b843559f23ee0eaf2ec5
evarela4mayor <eddievarelaformayor@gmail.com>
2025-07-13 17:51:23 -0600

    Update server.js

--- config/server.js
+++ config/server.js
@@ -2,4 +2,10 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'https://eddie-varela-cms.onrender.com'),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
 });
