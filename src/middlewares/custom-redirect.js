27245fc29b5cedba1b1e85a5070f6fd617880408
evarela4mayor <eddievarelaformayor@gmail.com>
2025-07-13 08:45:32 -0600

    Update custom-redirect.js
    Remove custom-redirect.js

--- src/middlewares/custom-redirect.js
+++ src/middlewares/custom-redirect.js
@@ -1,9 +1,1 @@
-module.exports = () => {
-  return async (ctx, next) => {
-    if (ctx.request.path === '/' || ctx.request.path === '') {
-      ctx.redirect('/admin');
-      return;
-    }
-    await next();
-  };
-};
+
