53c4e4f3fc0d2e5614ab08a448c40fdbe2eaf0c1
evarela4mayor <eddievarelaformayor@gmail.com>
2025-07-13 10:47:57 -0600

    Create custom-redirect.js

---
+++ src/middlewares/custom-redirect.js
@@ -0,0 +1,10 @@
+// src/middlewares/custom-redirect.js
+module.exports = () => {
+  return async (ctx, next) => {
+    if (ctx.request.path === '/' || ctx.request.path === '') {
+      ctx.redirect('/admin');
+      return;
+    }
+    await next();
+  };
+};
