8900479ade38f91b3bb3157252fc2167ac749360
evarela4mayor <eddievarelaformayor@gmail.com>
2025-07-12 20:41:04 -0600

    Update server.js
    Add URL.

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('STRAPI_PUBLIC_URL', 'https://eddie-varela-cms.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
