fce2267668ecb897c48284c0ff53951700a75926
evarela4mayor <eddievarelaformayor@gmail.com>
2025-07-13 18:09:05 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    allowedHosts: process.env.VITE_ALLOWED_HOSTS?.split(',') || ['localhost']
+    allowedHosts: 'all'
   }
 })
